12148a176665e0c447bdfcf58ce0fda69c9556b0
Anas Ansari <anasansari056789@gmail.com>
2025-10-09 22:26:23 +0530

    Initial commit

---
+++ apps/auth-service/src/swagger.js
@@ -0,0 +1,16 @@
+import swaggerAutogen from "swagger-autogen";
+
+const doc = {
+  info: {
+    title: "Auth service API",
+    description: "Automatically generated swagger docs",
+    version: "1.0.0",
+  },
+  host: "localhost:6001",
+  schemes: ["http"],
+};
+
+const outputFile = "./swagger-output.json";
+const endpointFiles = ["./routes/auth.router.ts"];
+
+swaggerAutogen()(outputFile, endpointFiles, doc);
