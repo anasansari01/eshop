03bc008b4b46823d4d009ab58630bbb4826ed1ab
Anas Ansari <anasansari056789@gmail.com>
2025-10-18 10:15:46 +0530

    Add the react-query to seller-ui

--- apps/seller-ui/src/app/layout.tsx
+++ apps/seller-ui/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './global.css';
+import Providers from './Providers';
 
 export const metadata = {
   title: 'Eshop Seller',
@@ -12,7 +13,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
     </html>
   )
 }
