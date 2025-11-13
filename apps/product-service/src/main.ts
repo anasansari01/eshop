5159b73f34e5c16a98417c50b9f8440df88f5129
Anas Ansari <anasansari056789@gmail.com>
2025-11-13 22:43:56 +0530

    setup path for product service for gateway

--- apps/product-service/src/main.ts
+++ apps/product-service/src/main.ts
@@ -2,8 +2,9 @@ import express from 'express';
 import cors from'cors';
 import cookieParser from 'cookie-parser';
 import { errorMiddleware } from '@packages/error-handler/error-middleware';
-// import swaggerUi from "swagger-ui-express"
-// const swaggerDocument = require("./swagger-output.json");
+import router from './routes/product.routes';
+import swaggerUi from "swagger-ui-express"
+const swaggerDocument = require("./swagger-output.json");
 
 const app = express();
 
@@ -16,27 +17,27 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-app.get('/product', (req, res) => {
+app.get('/api', (req, res) => {
     res.send({ 'message': 'Hello Product API'});
 });
 
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-// app.get("./docs-json", (req, res) =>{
-//     res.json(swaggerDocument);
-// });
+app.use("/product-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get("./docs-json", (req, res) =>{
+    res.json(swaggerDocument);
+});
 
 // Router
-// app.use("/api", router);
+app.use("/api", router);
 
 app.use(errorMiddleware);
 
 const port = process.env.PORT || 6002;
 
 const server = app.listen(port, ()=>{
-    console.log(`Product service is running at http://localhost:${port}/product`);
-    // console.log(`Swagger docs available at http://localhost:${port}/api-docs`);
+    console.log(`Product service is running at http://localhost:${port}/api`);
+    console.log(`Swagger docs available at http://localhost:${port}/product-docs`);
 });
 
 server.on("error", (err)=>{
     console.log("Server Error", err);
-})
+});
