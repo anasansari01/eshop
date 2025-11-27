f6c397d071ab8c1541a965fa534f3a8a483bbc68
Anas Ansari <anasansari056789@gmail.com>
2025-11-27 12:24:01 +0530

    add the routes for discount codes

--- apps/product-service/src/routes/product.routes.ts
+++ apps/product-service/src/routes/product.routes.ts
@@ -1,8 +1,12 @@
 import express, { Router } from 'express';
-import { getCategories } from '../controller/product.controller';
+import { createDiscountCode, deleteDiscountCodes, getCategories, getDiscountCodes } from '../controller/product.controller';
+import isAuthenticated from '@packages/middleware/isAuthenticated';
 
 const router: Router = express.Router();
 
 router.get("/get-categories", getCategories);
+router.post("/create-discount-code", isAuthenticated, createDiscountCode);
+router.get("/get-discount-codes", isAuthenticated, getDiscountCodes);
+router.delete("/delete-discount-code/:id", isAuthenticated, deleteDiscountCodes);
 
 export default router;
