7064ade8d38c89b1bf85fd17f663d96a6a1a73c4
Anas Ansari <anasansari056789@gmail.com>
2025-12-08 19:24:38 +0530

    add imagekit for image upload

---
+++ packages/libs/imagekit/index.ts
@@ -0,0 +1,9 @@
+// SDK initialization
+
+const ImageKit = require("imagekit");
+
+export const imagekit = new ImageKit({
+    publicKey : process.env.IMAGEKIT_PUBLIC_KEY!,
+    privateKey : process.env.IMAGEKIT_PRIVATE_KEY!,
+    urlEndpoint : "https://ik.imagekit.io/anasF60"
+});
