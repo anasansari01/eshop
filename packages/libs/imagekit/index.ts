56fa9ac3305d13410dad582e06a0aa35cb8c4809
Anas Ansari <anasansari056789@gmail.com>
2025-12-09 11:55:07 +0530

    update the imagekit file

--- packages/libs/imagekit/index.ts
+++ packages/libs/imagekit/index.ts
@@ -1,9 +1,5 @@
-// SDK initialization
-
-const ImageKit = require("imagekit");
+import ImageKit from "@imagekit/nodejs";
 
 export const imagekit = new ImageKit({
-    publicKey : process.env.IMAGEKIT_PUBLIC_KEY!,
-    privateKey : process.env.IMAGEKIT_PRIVATE_KEY!,
-    urlEndpoint : "https://ik.imagekit.io/anasF60"
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
 });
