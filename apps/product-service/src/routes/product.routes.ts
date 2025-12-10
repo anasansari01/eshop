60e848f6719dffa7526150f482b80d60f6bbeef7
Anas Ansari <anasansari056789@gmail.com>
2025-12-10 20:31:11 +0530

    add create product controller and router

--- apps/product-service/src/routes/product.routes.ts
+++ apps/product-service/src/routes/product.routes.ts
@@ -1,5 +1,5 @@
 import express, { Router } from 'express';
-import { createDiscountCode, deleteDiscountCodes, deleteProductImage, getCategories, getDiscountCodes, uploadProductImage } from '../controller/product.controller';
+import { createDiscountCode, createProduct, deleteDiscountCodes, deleteProductImage, getCategories, getDiscountCodes, uploadProductImage } from '../controller/product.controller';
 import isAuthenticated from '@packages/middleware/isAuthenticated';
 
 const router: Router = express.Router();
@@ -10,5 +10,6 @@ router.get("/get-discount-codes", isAuthenticated, getDiscountCodes);
 router.delete("/delete-discount-code/:id", isAuthenticated, deleteDiscountCodes);
 router.post("/upload-product-image", isAuthenticated, uploadProductImage);
 router.delete("/delete-product-image", isAuthenticated, deleteProductImage);
+router.post("/create-product", isAuthenticated, createProduct);
 
 export default router;
