266e7076ed5c6a1472b0bccdea2c2a41d70e005f
Anas Ansari <anasansari056789@gmail.com>
2025-12-11 16:14:54 +0530

    add the delete & restore mutation for product

--- apps/seller-ui/src/app/(routes)/dashboard/all-products/page.tsx
+++ apps/seller-ui/src/app/(routes)/dashboard/all-products/page.tsx
@@ -8,15 +8,23 @@ import {
   flexRender
 } from "@tanstack/react-table"
  
-import {Search, Pencil, Trash, Eye, Plus, BarChart, Star, ChevronRight, Pen} from "lucide-react"
+import {Search, Pencil, Trash, Eye, Plus, BarChart, Star, ChevronRight, Pen, RotateCcw} from "lucide-react"
 import Link from 'next/link'
 import axiosInstance from 'apps/seller-ui/src/utils/axiosInstance'
 import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query"
 import Image from 'next/image'
+import DeleteConfirmationModal from 'apps/seller-ui/src/shared/components/modals/delete.confirmation.modal';
 
 const fetchProducts = async () =>  {
   const res = await axiosInstance.get("/product/api/get-shop-products");
   return res?.data?.products;
+}
+
+const deleteProduct = async (productId: string) => {
+  await axiosInstance.delete(`/product/api/delete-product/${productId}`);
+}
+const restoreProduct = async (productId: string) => {
+  await axiosInstance.put(`/product/api/restore-product/${productId}`);
 }
 
 const ProductList = () => {
@@ -25,6 +33,7 @@ const ProductList = () => {
   const [showAnalytic, setshowAnalytic] = useState(false);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState<any>();
+  const queryClient = useQueryClient();
 
   const {data: products = [], isLoading} = useQuery({
     queryKey: ["shop-products"],
@@ -32,12 +41,30 @@ const ProductList = () => {
     staleTime: 1000 * 60 * 5,
   });
 
+  // delete product mutation
+  const deleteMutation = useMutation({
+    mutationFn: deleteProduct,
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ["shop-products"]});
+      setShowDeleteModal(false);
+    },
+  });
+
+  // restore product mutation
+  const restoreMutation = useMutation({
+    mutationFn: restoreProduct,
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ["shop-products"]});
+      setShowDeleteModal(false);
+    },
+  });
+
   const columns = useMemo(
     () => [
       {
         accessorKey: "image",
         header: "Image",
-        cell: ({row}: any) => <Image src={row.original?.image[0]?.url} alt={row.original?.image[0]?.url} width={200} height={200}  className='w-12 h-12 rounded-md object-cover'/>
+        cell: ({row}: any) => <Image src={row.original?.images[0]?.url} alt={row.original?.images[0]?.url} width={200} height={200}  className='w-12 h-12 rounded-md object-cover'/>
       },
       {
         accessorKey: "name",
@@ -91,30 +118,38 @@ const ProductList = () => {
         header: "Actions",
         cell: ({row}:any)=>(
           <div className='flex gap-3'>
-            <Link
-              href={`/product/${row.original.id}`}
-              className='text-blue-400 hover:text-blue-300 transition'
-            >
-              <Eye size={18}/>
-            </Link>
-            <Link 
-              href={`/product/edit/${row.original.id}`}
-              className='text-yellow-400 hover:text-yellow-300 transition'  
-            >
-              <Pencil size={18}/>
-            </Link>
-            <button 
-              className='text-green-400 hover:text-green-300 transition'
-              // onClick={()=>openAnalytics(row.original)}
-            >
-              <BarChart size={18}/>
-            </button>
-            <button 
-              className='text-red-400 hover:text-red-300 transition'
-              // onClick={()=>openDeleteModal(row.original)}
-            >
-              <Trash size={18}/>
-            </button>
+            {!row.original.isDeleted ? (
+              <>
+                <Link
+                  href={`/product/${row.original.id}`}
+                  className='text-blue-400 hover:text-blue-300 transition'
+                >
+                  <Eye size={18}/>
+                </Link>
+                <Link 
+                  href={`/product/edit/${row.original.id}`}
+                  className='text-yellow-400 hover:text-yellow-300 transition'  
+                >
+                  <Pencil size={18}/>
+                </Link>
+                <button 
+                  className='text-green-400 hover:text-green-300 transition'
+                  // onClick={()=>openAnalytics(row.original)}
+                >
+                  <BarChart size={18}/>
+                </button>
+                <button 
+                  className='text-red-400 hover:text-red-300 transition'
+                  onClick={()=>openDeleteModal(row.original)}
+                >
+                  <Trash size={18}/>
+                </button>
+              </>
+            ): (
+              <button onClick={()=>openDeleteModal(row.original)}>
+                <RotateCcw size={18} />
+              </button>
+            )}
           </div>
         ),
       },
@@ -129,6 +164,11 @@ const ProductList = () => {
     state: {globalFilter},
     onGlobalFilterChange: setGlobalFilter,
   })
+
+  const openDeleteModal = (product: any) => {
+    setSelectedProduct(product);
+    setShowDeleteModal(true);
+  }
 
 
   return (
@@ -198,8 +238,16 @@ const ProductList = () => {
             </tbody>
           </table>
         )}
-      </div>
 
+        {showDeleteModal && (
+          <DeleteConfirmationModal 
+            product={selectedProduct}
+            onClose={()=>setShowDeleteModal(false)}
+            onConfirm={()=>deleteMutation.mutate(selectedProduct?.id)}
+            onRestore={()=>restoreMutation.mutate(selectedProduct?.id)}
+            />
+        )}
+      </div>
     </div>
   )
 }
